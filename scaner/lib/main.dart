a71daef625f545118d3ec33534abc7ee6dddaaa6
Aya <ayamessaoudene2@gmail.com>
2025-03-07 01:07:32 +0000

    scan premier vertion

--- scaner/lib/main.dart
+++ scaner/lib/main.dart
@@ -1,125 +1,325 @@
 import 'package:flutter/material.dart';
+import 'package:barcode_widget/barcode_widget.dart';
+import 'package:mobile_scanner/mobile_scanner.dart'
+    as mobile_scanner; // Ajout d'un préfixe
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ScanPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ScanPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ScanPageState createState() => _ScanPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ScanPageState extends State<ScanPage> {
+  final TextEditingController _barcodeController = TextEditingController();
+  bool _isKeyPage = false; // Par défaut, on est dans la page SCAN
+  mobile_scanner.MobileScannerController _scannerController =
+      mobile_scanner.MobileScannerController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void dispose() {
+    _scannerController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.65,
+                padding: EdgeInsets.all(16.0), // Padding général
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 10.0,
+                      spreadRadius: 2.0,
+                    ),
+                  ],
+                ),
+                child: _isKeyPage ? _buildKeyPage() : _buildScanPage(),
+              ),
             ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.35,
+            color: Colors.grey[200],
+            padding: EdgeInsets.all(8.0),
+            child: _isKeyPage ? _buildKeypad() : Container(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildKeyPage() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(Icons.close, size: 24.0, color: Colors.grey),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'SCAN',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Opacity(
+              opacity: 0.0,
+              child: Icon(Icons.close, size: 24.0),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        SizedBox(height: 20.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _iconButton(Icons.qr_code_scanner, 'SCAN', isSelected: !_isKeyPage,
+                onTap: () {
+              setState(() {
+                _isKeyPage = false;
+              });
+            }),
+            _iconButton(Icons.apps, 'KEY', isSelected: _isKeyPage, onTap: () {
+              setState(() {
+                _isKeyPage = true;
+              });
+            }),
+          ],
+        ),
+        SizedBox(height: 50.0),
+        Text(
+          'Type the product barcode',
+          style: TextStyle(
+            fontSize: 16.0,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 10.0),
+        SizedBox(height: 80.0), // Espacement pour déplacer le code-barres
+        BarcodeWidget(
+          barcode: Barcode.code128(),
+          data: _barcodeController.text.padRight(13, '.'),
+          width: 300,
+          height: 80,
+          drawText: false, // Désactiver le texte intégré
+        ),
+        SizedBox(
+            height: 1.0), // Espacement réduit entre le code-barres et le texte
+        Text(
+          _barcodeController.text.padRight(13, '.').split('').join(' '),
+          style: TextStyle(
+            fontSize: 30.0, // Taille de police personnalisée
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
     );
+  }
+
+  Widget _buildScanPage() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(Icons.close, size: 24.0, color: Colors.grey),
+            Text(
+              'SCAN',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Opacity(
+              opacity: 0.0,
+              child: Icon(Icons.close, size: 24.0),
+            ),
+          ],
+        ),
+        SizedBox(height: 20.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _iconButton(Icons.qr_code_scanner, 'SCAN', isSelected: !_isKeyPage,
+                onTap: () {
+              setState(() {
+                _isKeyPage = false;
+              });
+            }),
+            _iconButton(Icons.apps, 'KEY', isSelected: _isKeyPage, onTap: () {
+              setState(() {
+                _isKeyPage = true;
+              });
+            }),
+          ],
+        ),
+        SizedBox(height: 20.0),
+        Expanded(
+          child: Column(
+            children: [
+              Expanded(
+                child: mobile_scanner.MobileScanner(
+                  controller: _scannerController,
+                  onDetect: (capture) {
+                    final List<mobile_scanner.Barcode> barcodes =
+                        capture.barcodes;
+                    for (final barcode in barcodes) {
+                      setState(() {
+                        _barcodeController.text = barcode.rawValue ?? '';
+                      });
+                    }
+                  },
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Scanned Barcode: ${_barcodeController.text}',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 10.0),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildKeypad() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        for (int i = 1; i <= 9; i += 3)
+          Row(
+            children: [
+              for (int j = i; j < i + 3; j++)
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: _buildKey('$j', _onNumberPressed),
+                  ),
+                ),
+            ],
+          ),
+        Row(
+          children: [
+            Expanded(
+              child: SizedBox(), // Espace vide sous le 7
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: _buildKey('0', _onNumberPressed),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: IconButton(
+                  icon:
+                      Icon(Icons.backspace, size: 30.0, color: Colors.black54),
+                  onPressed: () => _onClearPressed(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _iconButton(IconData icon, String label,
+      {bool isSelected = false, VoidCallback? onTap}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Icon(icon,
+              size: 30.0, color: isSelected ? Colors.black : Colors.black54),
+          SizedBox(height: 4.0),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 12.0,
+              color: isSelected ? Colors.black : Colors.black54,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildKey(String label, Function(String) onPressed) {
+    return ElevatedButton(
+      onPressed: () => onPressed(label),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        padding: EdgeInsets.symmetric(vertical: 10.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+      child: Text(label, style: TextStyle(color: Colors.black)),
+    );
+  }
+
+  void _onNumberPressed(String number) {
+    if (_barcodeController.text.length < 13) {
+      setState(() {
+        _barcodeController.text += number;
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Barcode must be 13 digits long')),
+      );
+    }
+  }
+
+  void _onClearPressed() {
+    if (_barcodeController.text.isNotEmpty) {
+      setState(() {
+        _barcodeController.text = _barcodeController.text
+            .substring(0, _barcodeController.text.length - 1);
+      });
+    }
+  }
+
+  void _onSubmitPressed(String _) {
+    if (_barcodeController.text.length == 13) {
+      print('Barcode: ${_barcodeController.text}');
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Barcode must be 13 digits long')),
+      );
+    }
   }
 }
